4f440a707237d4c88c07e6869d84ed1389158ae3
SiamNurnabi <siamnurnabi@gmail.com>
2025-02-21 11:49:45 +0600

    first commit

---
+++ src/main/java/com/example/taxraid/TaxRAidApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxraid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxRAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxRAidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taxraid/TaxRAidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxraid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxRAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
