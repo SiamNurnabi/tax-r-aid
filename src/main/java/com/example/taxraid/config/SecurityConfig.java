78daa125de1c93e8b3d7b17e4267749ce145c5fb
ahmad-rakib <ahmadrakib2017@gmail.com>
2025-02-22 11:02:02 +0600

    Basic security configuration

---
+++ src/main/java/com/example/taxraid/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.taxraid.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterSecurity(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/***").permitAll()
+                );
+        return http.build();
+    }
+}
