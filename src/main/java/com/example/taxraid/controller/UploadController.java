70a02b6713d4cb247fe653209a092e27dffc4875
ahmad-rakib <ahmadrakib2017@gmail.com>
2025-02-28 00:20:00 +0600

    Basic form submision for bank info

--- src/main/java/com/example/taxraid/controller/UploadController.java
+++ src/main/java/com/example/taxraid/controller/UploadController.java
@@ -1,15 +1,36 @@
 package com.example.taxraid.controller;
 
+import com.example.taxraid.entity.BankInformation;
+import com.example.taxraid.service.BankInformationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/upload")
 public class UploadController {
 
+    public final BankInformationService bankInformationService;
+
+    public UploadController(BankInformationService bankInformationService) {
+        this.bankInformationService = bankInformationService;
+    }
+
     @GetMapping
-    public String showUploadPage() {
+    public String showUploadPage(Model model) {
+
+        model.addAttribute("bankInformation", new BankInformation());
         return "upload";
+    }
+
+    @PostMapping("/add-bank-info")
+    public String submitBankInfo(@ModelAttribute("bankInformation") BankInformation bankInformation) {
+
+        bankInformationService.save(bankInformation);
+        return "redirect:/upload";
     }
 }
---
+++ src/main/java/com/example/taxraid/entity/BankInformation.java
@@ -0,0 +1,41 @@
+package com.example.taxraid.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class BankInformation extends BaseEntity {
+
+    @NotNull
+    @Column(name = "bank_name")
+    private String bankName;
+
+    @NotNull
+    @Column(name = "acc_no")
+    private Integer accNo;
+
+    @NotNull
+    @Column(name = "acc_type")
+    private String accType;
+
+    @NotNull
+    @Column(name = "opening_balance")
+    private Double openingBalance;
+
+    @NotNull
+    @Column(name = "interest")
+    private Double interest;
+
+    @NotNull
+    @Column(name = "tds")
+    private Double tds;
+
+    @NotNull
+    @Column(name = "balance")
+    private Double balance;
+}
