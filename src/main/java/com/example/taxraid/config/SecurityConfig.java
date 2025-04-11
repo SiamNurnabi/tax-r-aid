769855071c4d18292b9e2976e594aa9e9755baf0
SiamNurnabi <siamnurnabi@gmail.com>
2025-04-11 21:40:29 +0600

    Custom login page is showing. After login redirecting to profile page.

--- src/main/java/com/example/taxraid/config/SecurityConfig.java
+++ src/main/java/com/example/taxraid/config/SecurityConfig.java
@@ -3,8 +3,11 @@ package com.example.taxraid.config;
 import com.example.taxraid.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,50 +18,46 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
-    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+    public SecurityConfig(CustomUserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
         this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-//    @Bean
-//    public SecurityFilterChain filterSecurity(HttpSecurity http) throws Exception {
-//        http.csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(authorizeRequests ->
-//                        authorizeRequests
-//                                .requestMatchers("/**").permitAll()
-//                );
-//        return http.build();
-//    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         req ->
-                                req
-                                        .requestMatchers("/register/**").permitAll() //
-                                        .anyRequest().authenticated())     // any request must be authenticated
-                .formLogin(Customizer.withDefaults())      // enable form based auth with default settings
-                .logout(Customizer.withDefaults())
-                .userDetailsService(userDetailsService);
+                        {
+                            req.requestMatchers("/register/**", "/main.css").permitAll();
+                            req.anyRequest().authenticated();
+                        })
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .loginProcessingUrl("/authenticate")
+                        .defaultSuccessUrl("/profile", true)
+                        .failureForwardUrl("/login?error=true")
+                        .usernameParameter("userName")
+                        .passwordParameter("password")
+                        .permitAll())
+                .logout(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
-    //    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails userDetails = User
-//                .withUsername("siam")
-//                .password(passwordEncoder().encode("12345"))
-//                .roles("ADMIN")
-//                .build();
-//        return new InMemoryUserDetailsManager(userDetails);
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 }
